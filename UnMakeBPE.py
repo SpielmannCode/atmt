cdb5f7044c6e9cc649890f5e0868c9d011d25079
SpielmannCode <23359516+SpielmannCode@users.noreply.github.com`>
2019-11-11 19:00:47 +0100

    Changes

--- UnMakeBPE.py
+++ UnMakeBPE.py
@@ -6,8 +6,8 @@ sp = spm.SentencePieceProcessor()
 
 sp.load('joint.model')
 
-with open('model_translations.txt', 'r', encoding="utf-8") as f:
-    target = open('model_translations.out', 'w', encoding="utf-8")
+with open('translations/BPE/model_translations.txt', 'r', encoding="utf-8") as f:
+    target = open('translations/BPE/model_translations.out', 'w', encoding="utf-8")
     for line in f:
         BPE = line.split()
         target.write((sp.decode_pieces(BPE)) + '\n')
