ac03e94036b4503825c84e342671de666a5a3e1e
SpielmannCode <23359516+SpielmannCode@users.noreply.github.com`>
2019-11-12 14:40:00 +0100

    Added Translation of BPE no Cuda

--- UnMakeBPE.py
+++ UnMakeBPE.py
@@ -6,8 +6,8 @@ sp = spm.SentencePieceProcessor()
 
 sp.load('joint.model')
 
-with open('translations/BPE_Batch1_cuda/model_translations.txt', 'r', encoding="utf-8") as f:
-    target = open('translations/BPE_Batch1_cuda/model_translations.out', 'w', encoding="utf-8")
+with open('translations/BPE_Batch1/model_translations.txt', 'r', encoding="utf-8") as f:
+    target = open('translations/BPE_Batch1/model_translations.out', 'w', encoding="utf-8")
     for line in f:
         BPE = line.split()
         target.write((sp.decode_pieces(BPE)) + '\n')
